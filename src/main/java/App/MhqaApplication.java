86b5a1fa42d57508106cce3bedea0b242d2d9fe2
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-12-12 10:32:22 -0400

    esqueleto de projeto
    Diretórios inseridos

---
+++ src/main/java/App/MhqaApplication.java
@@ -0,0 +1,13 @@
+package App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhqaApplication.class, args);
+	}
+
+}
